01904fb23c596d93bf80921e66a060cd6592abea
Altamash Shaikh <mr.shaikh.altamash0203@gmail.com>
2025-03-04 23:48:11 +0530

    Q11 answer

---
+++ Into to git  & github/Q11-reversenum.js
@@ -0,0 +1,16 @@
+
+function revnum(num)
+{
+    let snum=String(num)
+    let reversednum=""
+    for(i=snum.length-1;i>=0;i--)
+       
+        {
+           reversednum+=snum[i]
+        }
+        let num1=reversednum
+        console.log(num1)
+
+
+}
+revnum(971)
