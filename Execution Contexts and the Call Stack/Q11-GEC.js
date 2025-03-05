58efc0f6874e672b1893488a49d677920ff1f820
Altamash Shaikh <mr.shaikh.altamash0203@gmail.com>
2025-03-05 19:56:56 +0530

    understanding GEC

---
+++ Execution Contexts and the Call Stack/Q11-GEC.js
@@ -0,0 +1,15 @@
+var age=19
+
+function displayage()
+{
+    console.log(age)
+    
+    function chnageage()
+    {
+        age=20
+        console.log(age)
+    }
+    chnageage()
+}
+
+displayage()
